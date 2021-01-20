bc3516d09e9daa2ccc295baf39cecc28034e5631
Jiachen Ren <jiachenren@gmail.com>
2021-01-20 16:11:56 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 # Manually fix flutter beta web packaging error
 if [[ -d "build/web/assets/assets/images" ]]
 then
-  rm -rf build/web/assets/images
+  if [[ -d "build/web/assets/images" ]]
+  then
+    rm -rf build/web/assets/images
+  fi
   mv build/web/assets/assets/images build/web/assets/
 fi
 
